39eadd9fa92b11e039b83057978e4f4bad7ea20a
Jondrewd <jonas220703@gmail.com>
2024-01-08 17:43:52 -0300

    logica programação

--- Data.java
+++ Data.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,13 +21,23 @@ public class Data {
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy").withZone(ZoneId.systemDefault()); 
 		
-		System.out.println("d01 = " + d01.format(fmt1));
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		/*System.out.println("d01 = " + d01.format(fmt1));
 		System.out.println("d02 = " + d02.toString());
 		System.out.println("d03 = " + d03.toString());
 		System.out.println("d04 = " + d04.toString());
 		System.out.println("d05 = " + d05.toString());
 		System.out.println("d06 = " + fmt2.format(d06));
 		System.out.println("d07 = " + d07.toString());
+		System.out.println(r1);
+		System.out.println(r2);*/
+		
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		Duration d1 = Duration.between(r1.atTime(0, 0) , d01.atTime(0, 0));
+				System.out.println("Duration = " + d1);
+				
 	}
 
 }
