400cb99758842cd94011b968b26aa59828bef932
Jondrewd <jonas220703@gmail.com>
2024-03-12 10:52:49 -0300

    Update App.java

--- Exercicios/Exercicio7/Ex7/src/App.java
+++ Exercicios/Exercicio7/Ex7/src/App.java
@@ -1,19 +1,62 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Produto;
 
 public class App {
     public static void main(String[] args) {
-        String[] lines = new String[]{"Good Morning", "Good Afternoon", "Good Night"};
-        String path = "c:\\tmp\\out.txt";
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
-            for (String line : lines){
-                bw.write(line);
-                bw.newLine();
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Produto> list = new ArrayList<>();
+
+        System.out.println("Entre com o caminho do arquivo: ");
+        String path = sc.nextLine();
+        //  src\input.csv
+
+        File file = new File(path);
+        String sourceFolder = file.getParent();
+
+        boolean sucess = new File(sourceFolder + "\\out").mkdir();
+        
+        String targetFile = sourceFolder + "\\out\\sumary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            
+            String item = br.readLine();
+            while(item != null){
+                String[] field = item.split(",");
+                String name = field[0];
+                double price = Double.parseDouble(field[1]);
+                int quantity = Integer.parseInt(field[2]);
+                
+                list.add(new Produto(name, price, quantity));
+                
+                item = br.readLine();
             }
-        }
-        catch(IOException e){
-            e.printStackTrace();
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
+                for (Produto produto : list) {
+                    bw.write(produto.getName()+ ", "+ String.format("%.2f", 
+                    produto.totalValue()));
+                    bw.newLine();
+                }
+
+                
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error");
         }
     }
 }
