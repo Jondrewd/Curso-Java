85797cd93ba8174cb5ef3b7e80249f1de5fa289b
Jondrewd <jonas220703@gmail.com>
2024-03-17 19:49:04 -0300

    iniciando exercicio

--- Exercicios/Exercicio9/Ex9/src/App.java
+++ Exercicios/Exercicio9/Ex9/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.print("Numero: ");
+
+        System.out.print("Data (dd/MM/yyyy): ");
+
+        System.out.print("Valor do contrato: ");
+
+        System.out.print("Entre com os numeros de parcelas: ");
+
+        System.out.println("Parcelas:");
+
+
+        sc.close();
     }
 }
